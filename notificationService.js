164afb86b0dd323aa8183b2af26a4f74e1a7db83
Parimal Ramkrushna Salunkhe <113320185+ParimalRSalunkhe@users.noreply.github.com>
2024-06-17 07:43:49 +0530

    Create notificationService.js

---
+++ notificationService.js
@@ -0,0 +1,5 @@
+const sendEmailNotification = async (email, subject, message) => {
+  // Logic to send email (e.g., using Nodemailer or an email API)
+};
+
+module.exports = { sendEmailNotification };
